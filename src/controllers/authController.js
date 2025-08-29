9b1ed184d20bb8d7c1d57fefe05ba80ad7939e3b
sushantsjamdade-create <sushantsjamdade@gmail.com>
2025-08-29 14:01:29 +0530

    console removed

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -93,7 +93,7 @@ exports.login = async (req, res) => {
         });
 
     } catch (err) {
-        console.error('Login error:', err);
+       
         res.status(500).json({ message: 'Internal server error' });
     }
 };

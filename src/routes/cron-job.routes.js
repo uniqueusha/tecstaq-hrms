160f1a88316cfba3e183b141f873c359fa61b941
uniqueusha <ushamyadav777@gmail.com>
2026-05-05 17:26:24 +0530

    birthday email

--- src/routes/cron-job.routes.js
+++ src/routes/cron-job.routes.js
@@ -2,5 +2,6 @@ const express = require("express");
 const router = express.Router();
 const cronJobController = require('../controllers/cron-job.controller')
 router.get('/auto-check-out', cronJobController.autoCheckOut);
+router.get('/birth-email', cronJobController.empEmail);
 
 module.exports = router
